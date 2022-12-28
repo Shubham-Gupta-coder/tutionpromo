9aacc7d917f2ae208c6448898aa6a9c670a2588e
Shubham <shubhamguptakrj2008@gmail.com>
2022-12-28 13:29:02 +0530

    Video7

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,6 @@ import Head from 'next/head'
 // #f90588,#5a2776
 import { FaChalkboardTeacher, FaChild } from 'react-icons/fa'
 import { BiBuildingHouse } from 'react-icons/bi'
-
 export default function Home() {
   return (
     <>
@@ -43,7 +42,7 @@ export default function Home() {
               </div>
             </div>
           </div>
-          <section className="text-gray-600 my-10 body-font">
+          <section className="text-gray-600 my-40 mb-20 body-font">
             <div className="container px-5 py-24 mx-auto">
               <div className="flex flex-col text-center w-full mb-20">
                 <h1 className="text-6xl font-medium title-font my-4 text-gray-900">Need A Reason To Trust ?</h1>
@@ -80,6 +79,68 @@ export default function Home() {
                     <BiBuildingHouse className='text-4xl my-3 text-[#f90588]' />
                     <h2 className="title-font font-medium text-3xl text-gray-900">46</h2>
                     <p className="leading-relaxed">Places</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section className="text-gray-600 body-font relative">
+            <div className="container px-5 py-24 mx-auto">
+              <div className="flex flex-col text-center w-full mb-12">
+                <h1 className="text-6xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
+                <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Book a Tution Class For Your Child Now !</p>
+              </div>
+              <div className="lg:w-1/2 md:w-2/3 mx-auto">
+                <div className="flex flex-wrap -m-2">
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label for="name" className="leading-7 text-sm text-gray-600">Name</label>
+                      <input type="text" id="name" name="name" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                  </div>
+                  <div className="p-2 w-1/2">
+                    <div className="relative">
+                      <label for="email" className="leading-7 text-sm text-gray-600">Email</label>
+                      <input type="email" id="email" name="email" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+                    </div>
+                  </div>
+                  <div className="p-2 w-full">
+                    <div className="relative">
+                      <label for="message" className="leading-7 text-sm text-gray-600">Address</label>
+                      <textarea id="message" name="message" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-pink-500 focus:bg-white focus:ring-2 focus:ring-pink-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                    </div>
+                  </div>
+                  <div className="p-2 w-full">
+                    <button className="flex mx-auto text-white bg-pink-500 border-0 py-2 px-8 focus:outline-none hover:bg-pink-600 rounded text-lg">Submit</button>
+                  </div>
+                  <div className="p-2 w-full pt-8 mt-8 border-t border-gray-200 text-center">
+                    <a className="text-pink-500">megatutions@email.com</a>
+                    <p className="leading-normal my-5">49 Smith St.
+                      <br/>Saint Cloud, MN 56301
+                    </p>
+                    <span className="inline-flex">
+                      <a className="text-gray-500">
+                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-4 text-gray-500">
+                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                          <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-4 text-gray-500">
+                        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                          <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                          <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                        </svg>
+                      </a>
+                      <a className="ml-4 text-gray-500">
+                        <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
+                        </svg>
+                      </a>
+                    </span>
                   </div>
                 </div>
               </div>
